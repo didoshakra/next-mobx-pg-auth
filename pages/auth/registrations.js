0b9c58e3a096a0e6899299b6156bef9c62dad570
Roman <didoshakr@ukr.net>
2023-03-18 19:40:34 +0200

    beg7

---
+++ pages/auth/registrations.js
@@ -0,0 +1,9 @@
+//Registrations.js
+
+export default function Registrations() {
+  return (
+    <div>
+      <h1 style={{ display: "flex", color: "red", alignItems: "center", justifyContent: "center" }}>Registrations</h1>
+    </div>
+  )
+}
---
+++ pages/auth/singin.js
@@ -0,0 +1,10 @@
+//singin.js
+export default function Singin() {
+  return (
+    <div>
+      <h1 style={{ display: "flex", color: "red", alignItems: "center", justifyContent: "center" }}>singin</h1>
+    </div>
+  )
+}
+
+
