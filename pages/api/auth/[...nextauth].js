1d48c5e0bc162dc30a408f20bdaae170065d8c13
Roman <didoshakr@ukr.net>
2023-03-18 19:06:02 +0200

    уу

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,23 +13,10 @@ export const authOptions = {
   secret: process.env.NextAuth_SECRET,
   // Налаштувати один або більше постачальників автентифікації
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
+    // EmailProvider({
+    //      server: process.env.EMAIL_SERVER,
+    //      from: process.env.EMAIL_FROM,
+    //    }),
 
     GithubProvider({
       clientId: process.env.GITHUB_ID,
